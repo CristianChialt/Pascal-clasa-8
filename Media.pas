29ec1ddf293e9e5d910db025e6efd1c8ee2b6fdd
Cristian Chilat <chilatcristian@gmail.com>
2021-05-19 19:49:22 +0300

    Loaded Media.pas

---
+++ Media.pas
@@ -0,0 +1,22 @@
+﻿Program Medie;
+
+var 
+  a: integer;
+  b: integer;
+  c: integer;
+  d: integer;
+
+begin
+
+  writeln('Scrieti patru numere pentru a afla media lor - '); 
+  write('a = ');
+  readln(a);
+  write('b = ');
+  readln(b);
+  write('c = ');
+  readln(c);
+  write('d = ');
+  readln(d);
+  write('Media = ', (a + b + c + d) / 4)
+
+end.
