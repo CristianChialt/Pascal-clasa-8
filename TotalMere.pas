0afca94c136dc8a2a22c988aa4a434c871857448
Cristian Chilat <chilatcristian@gmail.com>
2021-05-19 20:04:11 +0300

    Loaded TotatMere.pas

---
+++ TotalMere.pas
@@ -0,0 +1,23 @@
+﻿program TotalMere;
+
+var
+   applesMaria: word;
+   applesIoana: word;
+   applesMihai: word;
+   totalApples: word;
+   z: word;
+   
+begin
+
+  writeln('Cite mere are Ioana?');
+  readln(applesIoana);
+  writeln('Cu cite mere mai mult are Maria decit Ioana?');
+  readln(z);
+  applesMaria := applesIoana + z;
+  writeln('Maria are ', applesMaria,' mere');
+  applesMihai := 2 * applesMaria;
+  writeln('Mihai are ', applesMihai, ' mere');
+  totalApples := applesIoana + applesMaria + applesMihai;
+  writeln('In total copii au ', totalApples, ' mere');
+
+end.
