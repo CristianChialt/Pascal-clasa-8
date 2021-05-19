79c0dd0343dc675adbf9d621b90f41a35b9f9b2a
Cristian Chilat <chilatcristian@gmail.com>
2021-05-19 20:30:43 +0300

    Loaded TimpulCalatoriei.pas

---
+++ TimpulCalatoriei.pas
@@ -0,0 +1,32 @@
+﻿program TimpulCalatoriei;
+
+var
+  departureHour: 0..23;
+  departureMinute: 0..59;
+  arrivalHour: 0..23;
+  arrivalMinute: 0..59;
+  travelTimeInMinutes: 0..1440; 
+
+begin
+
+  writeln('Introduceti timpul plecarii ');
+  write('Ora = ');
+  readln(departureHour);
+  write('Minuta = ');
+  readln(departureMinute);
+  
+  writeln('Introduceti timpul sosirii');
+  write('Ora = ');
+  readln(arrivalHour);
+  write('Minuta = ');
+  readln(arrivalMinute);
+  
+  travelTimeInMinutes := (arrivalHour * 60 + arrivalMinute) - (departureHour * 60 + departureMinute);
+  if(travelTimeInMinutes > 0) then
+  begin
+  writeln('Timpul calatoriei este de', (travelTimeInMinutes div 60), ':', (travelTImeInMinutes mod 60));
+  end1
+  else
+  writeln('Biletul dumneavoastra este invalid')
+  
+end.
