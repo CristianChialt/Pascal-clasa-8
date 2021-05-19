6ccd121f74cc6096c1f5b13c9c3c575a7e8f7db0
Cristian Chilat <chilatcristian@gmail.com>
2021-05-19 19:40:08 +0300

    Loaded Pitagora.pas

---
+++ Pitagora.pas
@@ -0,0 +1,19 @@
+﻿program Pitagora;
+
+var 
+  firstCathetus: word;
+  secondCathetus: word;
+  hypotenuse: real;
+
+begin 
+  write('Introduceti prima cateta = '); 
+  readln(firstCathetus);
+  write('Introduceti prima cateta = ');
+  readln(secondCathetus);
+  writeln('Prima cateta = ', firstCathetus);
+  writeln('A doua cateta = ', secondCathetus);
+  writeln('====== REZULTAT ======');
+  hypotenuse := sqrt(sqr(firstCathetus) + sqr(secondCathetus));
+  writeln('Ipotenuza = ', hypotenuse);
+
+end.
